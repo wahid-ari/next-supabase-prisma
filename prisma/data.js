afdfd0c3ad223e14b730870dbd00bcf9143df4c7
wahidari <wahiid.ari@gmail.com>
2023-03-23 01:01:23 +0700

    created to store json data

---
+++ prisma/data.js
@@ -0,0 +1,382 @@
+const album = [
+  {
+    "id": 1,
+    "name": "Bintang di Surga",
+    "cover": "https://i.scdn.co/image/ab67616d00001e02af69d9e82cc973f608481610",
+    "artists_id": 1
+  },
+  {
+    "id": 2,
+    "name": "Hari yang Cerah",
+    "cover": "https://i.scdn.co/image/ab67616d00001e02076747ead143aa3cd8c1d2f0",
+    "artists_id": 1
+  },
+  {
+    "id": 3,
+    "name": "Taman Langit",
+    "cover": "https://i.scdn.co/image/ab67616d0000b27324496b7707e9304c4ad76c5a",
+    "artists_id": 1
+  },
+  {
+    "id": 4,
+    "name": "Seperti Seharusnya",
+    "cover": "https://i.scdn.co/image/ab67616d00001e02f99e19baa3e57fd03b24ff5d",
+    "artists_id": 1
+  },
+  {
+    "id": 5,
+    "name": "Keterkaitan Keterikatan",
+    "cover": "https://i.scdn.co/image/ab67616d00001e02d6342c75204c065e554ede10",
+    "artists_id": 1
+  },
+  {
+    "id": 6,
+    "name": "Appetite for Destruction",
+    "cover": "https://i.scdn.co/image/ab67616d0000b27368384dd85fd5e95831252f60",
+    "artists_id": 2
+  },
+  {
+    "id": 7,
+    "name": "Use Your Illusion I",
+    "cover": "https://i.scdn.co/image/ab67616d00001e022ab85e10aa698513679ff509",
+    "artists_id": 2
+  },
+  {
+    "id": 8,
+    "name": "GNR Lies",
+    "cover": "https://i.scdn.co/image/ab67616d00001e02d2c9d673548c12ad1c32e38d",
+    "artists_id": 2
+  },
+  {
+    "id": 9,
+    "name": "A Head Full of Dreams",
+    "cover": "https://i.scdn.co/image/ab67616d00001e028ff7c3580d429c8212b9a3b6",
+    "artists_id": 3
+  },
+  {
+    "id": 10,
+    "name": "X&Y",
+    "cover": "https://i.scdn.co/image/ab67616d0000b2734e0362c225863f6ae2432651",
+    "artists_id": 3
+  },
+  {
+    "id": 11,
+    "name": "Parachutes",
+    "cover": "https://i.scdn.co/image/ab67616d00001e023d92b2ad5af9fbc8637425f0",
+    "artists_id": 3
+  },
+  {
+    "id": 12,
+    "name": "A Rush of Blood to the Head",
+    "cover": "https://i.scdn.co/image/ab67616d00001e02de09e02aa7febf30b7c02d82",
+    "artists_id": 3
+  },
+  {
+    "id": 13,
+    "name": "Mylo Xyloto",
+    "cover": "https://i.scdn.co/image/ab67616d0000b273de0cd11d7b31c3bd1fd5983d",
+    "artists_id": 3
+  },
+  {
+    "id": 14,
+    "name": "OST. Alexandria",
+    "cover": "https://i.scdn.co/image/ab67616d00001e02d945ace4a7fb61330e08a912",
+    "artists_id": 1
+  },
+  {
+    "id": 15,
+    "name": "Second Chance",
+    "cover": "https://i.scdn.co/image/ab67616d0000b27391079d7ff840d5273aa2957d",
+    "artists_id": 1
+  },
+  {
+    "id": 16,
+    "name": "Use Your Illusion II",
+    "cover": "https://i.scdn.co/image/ab67616d0000b27392d21aef6c0d288cc4c05973",
+    "artists_id": 2
+  },
+  {
+    "id": 17,
+    "name": "Best of Kshmr",
+    "cover": "https://i.scdn.co/image/ab67616d00001e02da13398baef8f339131bbbac",
+    "artists_id": 4
+  },
+  {
+    "id": 18,
+    "name": "Sheila On 7",
+    "cover": "https://i.scdn.co/image/ab67616d00001e0254270208627aa8061a8abe21",
+    "artists_id": 5
+  },
+  {
+    "id": 19,
+    "name": "Kisah Klasik Untuk Masa Depan",
+    "cover": "https://i.scdn.co/image/ab67616d00001e02e8d4f0596d66ccbe5241918d",
+    "artists_id": 5
+  },
+  {
+    "id": 20,
+    "name": "07 Des",
+    "cover": "https://i.scdn.co/image/ab67616d00001e0258961269cc5263913f9e0b40",
+    "artists_id": 5
+  },
+  {
+    "id": 21,
+    "name": "Pejantan Tangguh",
+    "cover": "https://i.scdn.co/image/ab67616d00001e02f0a7efc94687cc049b821529",
+    "artists_id": 5
+  },
+  {
+    "id": 22,
+    "name": "Menentukan Arah",
+    "cover": "https://i.scdn.co/image/ab67616d00001e025974be9bbf90d8f21af5515f",
+    "artists_id": 5
+  },
+  {
+    "id": 23,
+    "name": "Berlayar",
+    "cover": "https://i.scdn.co/image/ab67616d00001e02505fb06705d4823830ce5055",
+    "artists_id": 5
+  },
+  {
+    "id": 24,
+    "name": "Musim Yang Baik",
+    "cover": "https://i.scdn.co/image/ab67616d00001e02eb44a67b266f6987e21a4a9b",
+    "artists_id": 5
+  },
+  {
+    "id": 25,
+    "name": "OST. 30 Hari Mencari Cinta",
+    "cover": "https://i.scdn.co/image/ab67616d00001e0298b7cd420705531db32b3aeb",
+    "artists_id": 5
+  },
+  {
+    "id": 26,
+    "name": "Dewa 19",
+    "cover": "https://i.scdn.co/image/ab67616d00001e02d8fbd1079c6cfab4d4b70cea",
+    "artists_id": 6
+  },
+  {
+    "id": 27,
+    "name": "Format Masa Depan",
+    "cover": "https://i.scdn.co/image/ab67616d00001e02bfc7fa507f4bc822cabe7bde",
+    "artists_id": 6
+  },
+  {
+    "id": 28,
+    "name": "Terbaik Terbaik",
+    "cover": "https://i.scdn.co/image/dbb127abf2d9f53acf344c6461fb8ae23fdd1730",
+    "artists_id": 6
+  },
+  {
+    "id": 29,
+    "name": "Pandawa Lima",
+    "cover": "https://i.scdn.co/image/86d5fb0ee073484e45ce0544af78be82b8943024",
+    "artists_id": 6
+  },
+  {
+    "id": 30,
+    "name": "The 2000 Greatest",
+    "cover": "https://i.scdn.co/image/ab67616d00001e02893d2e279513cf125d385b06",
+    "artists_id": 6
+  },
+  {
+    "id": 31,
+    "name": "Laguku",
+    "cover": "https://i.scdn.co/image/ab67616d00001e027d3cd831a61fb7cea66bf174",
+    "artists_id": 7
+  },
+  {
+    "id": 32,
+    "name": "Melayang",
+    "cover": "https://i.scdn.co/image/ab67616d00001e020f1c818c3d95a460cef8410f",
+    "artists_id": 7
+  },
+  {
+    "id": 33,
+    "name": "Ruang Hati",
+    "cover": "https://i.scdn.co/image/ab67616d00001e02ef6821c358fd21c00921d919",
+    "artists_id": 7
+  },
+  {
+    "id": 34,
+    "name": "Truth, Cry, And Lie",
+    "cover": "https://i.scdn.co/image/ab67616d0000b2739a2a476d6cd4500f26fc484f",
+    "artists_id": 8
+  },
+  {
+    "id": 35,
+    "name": "Donâ€™t Make Me Sad",
+    "cover": "https://i.scdn.co/image/ab67616d00001e02beb3148bd8f70f1f62771d0a",
+    "artists_id": 8
+  }
+]
+
+const artist = [
+  {
+    "id": 1,
+    "name": "Noah",
+    "cover_url": "https://i.scdn.co/image/ab6761610000e5eba8fe25ec607f3cca02f5db6c",
+    "genre_id": 1
+  },
+  {
+    "id": 2,
+    "name": "Guns n Roses",
+    "cover_url": "https://i.scdn.co/image/ab67616d0000b2734e04404c40be7b80855656a8",
+    "genre_id": 2
+  },
+  {
+    "id": 3,
+    "name": "Coldplay",
+    "cover_url": "https://i.scdn.co/image/ab67616d00001e02b151437d4adc97ce6c828d4a",
+    "genre_id": 3
+  },
+  {
+    "id": 4,
+    "name": "Kshmr",
+    "cover_url": "https://i.scdn.co/image/ab6761610000e5ebd1db941d6e307ab229651fae",
+    "genre_id": 4
+  },
+  {
+    "id": 5,
+    "name": "Sheila On 7",
+    "cover_url": "https://i.scdn.co/image/ab67616d0000b27354270208627aa8061a8abe21",
+    "genre_id": 1
+  },
+  {
+    "id": 6,
+    "name": "Dewa 19",
+    "cover_url": "https://i.scdn.co/image/ab6761610000e5eb96bfa9ff288138f3432e0fa4",
+    "genre_id": 2
+  },
+  {
+    "id": 7,
+    "name": "Ungu",
+    "cover_url": "https://i.scdn.co/image/ab6761610000e5eb2d4637222eabdc240d5134d4",
+    "genre_id": 1
+  },
+  {
+    "id": 8,
+    "name": "Letto",
+    "cover_url": "https://i.scdn.co/image/ab6761610000e5eba72d9867fae5d76011bb2204",
+    "genre_id": 1
+  }
+]
+
+const genre = [
+  {
+    "id": 1,
+    "name": "Pop"
+  },
+  {
+    "id": 2,
+    "name": "Rock"
+  },
+  {
+    "id": 3,
+    "name": "Alternative"
+  },
+  {
+    "id": 4,
+    "name": "Electronic"
+  }
+]
+
+const song = [
+  {
+    "id": 1,
+    "name": "Hari Yang Cerah",
+    "youtube_url": "fuEdMCclYn0",
+    "cover_url": "https://i.scdn.co/image/ab67616d00001e02076747ead143aa3cd8c1d2f0",
+    "artist_id": 1,
+    "album_id": 2,
+    "preview_url": "https://p.scdn.co/mp3-preview/08bd5940bc2031c8c93306cccb32d5b9756fdc53"
+  },
+  {
+    "id": 2,
+    "name": "Bintang Di Surga",
+    "youtube_url": "B1ynHmn0XZ4",
+    "cover_url": "https://i.scdn.co/image/ab67616d00001e02af69d9e82cc973f608481610",
+    "artist_id": 1,
+    "album_id": 1,
+    "preview_url": "https://p.scdn.co/mp3-preview/09474fc657c15038cb699afa4a52b4fac9383d62"
+  },
+  {
+    "id": 3,
+    "name": "Taman Langit",
+    "youtube_url": "EzcuhqJKlLk",
+    "cover_url": "https://i.scdn.co/image/ab67616d0000b27324496b7707e9304c4ad76c5a",
+    "artist_id": 1,
+    "album_id": 3,
+    "preview_url": "https://p.scdn.co/mp3-preview/f2909fb81ec28143e1dbf032a0e8a4fa5d5d1837"
+  },
+  {
+    "id": 4,
+    "name": "Separuh Aku",
+    "youtube_url": "b0ZBBjViV8Y",
+    "cover_url": "https://i.scdn.co/image/ab67616d0000b273aa994128eba603922ce924a0",
+    "artist_id": 1,
+    "album_id": 4,
+    "preview_url": "https://p.scdn.co/mp3-preview/2c38cc2b48dfb3884a298c5b81f238947328fed0"
+  },
+  {
+    "id": 5,
+    "name": "My Situation",
+    "youtube_url": "aYPXy6kE1GA",
+    "cover_url": "https://i.scdn.co/image/ab67706c0000bebb8d82e2ee27cd5b4018e5f480",
+    "artist_id": 1,
+    "album_id": 5,
+    "preview_url": "https://p.scdn.co/mp3-preview/0162f5cddc2739b36cbaa4e02508fa23eddb1beb"
+  },
+  {
+    "id": 6,
+    "name": "Sweet Child of Mine",
+    "youtube_url": "1w7OgIMMRc4",
+    "cover_url": "https://i.scdn.co/image/ab67616d0000b273e44963b8bb127552ac761873",
+    "artist_id": 2,
+    "album_id": 6,
+    "preview_url": "https://p.scdn.co/mp3-preview/92ac4ba7bc8ed36373b795b5b9ffe7e37468cc4f"
+  },
+  {
+    "id": 7,
+    "name": "November Rain",
+    "youtube_url": "8SbUC-UaAxE",
+    "cover_url": "https://i.scdn.co/image/ab67616d0000b27368384dd85fd5e95831252f60",
+    "artist_id": 2,
+    "album_id": 7,
+    "preview_url": "https://p.scdn.co/mp3-preview/b7a92e09a950694e1c0e6fe644d68b6932bc47ee"
+  },
+  {
+    "id": 8,
+    "name": "Dont Cry",
+    "youtube_url": "zRIbf6JqkNc",
+    "cover_url": "https://i.scdn.co/image/ab67616d0000b273bdba586eb69c503f7ff7d658",
+    "artist_id": 2,
+    "album_id": 7,
+    "preview_url": "https://p.scdn.co/mp3-preview/34a693fa6d6a033cb0b2f8b4828a0e6781db7729"
+  },
+  {
+    "id": 9,
+    "name": "Patience",
+    "youtube_url": "ErvgV4P6Fzc",
+    "cover_url": "https://i.scdn.co/image/ab67706c0000da8483d2a5bf0a60bf11cd408a3b",
+    "artist_id": 2,
+    "album_id": 8,
+    "preview_url": "https://p.scdn.co/mp3-preview/8103cc39818fb7b70f1787e2ac23c73e893ed037"
+  },
+  {
+    "id": 10,
+    "name": "Paradise City",
+    "youtube_url": "Rbm6GXllBiw",
+    "cover_url": "https://i.scdn.co/image/ab67616d00001e027e1774622a9c5f52b7dd1c33",
+    "artist_id": 2,
+    "album_id": 6,
+    "preview_url": "https://p.scdn.co/mp3-preview/d10d5bfdfda3bd36a6a99c5cbc9d3561d82c60ba"
+  }
+]
+
+module.exports = {
+  album,
+  artist,
+  genre,
+  song
+};
