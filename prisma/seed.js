15d6d1a82edc389429fc2b04af41c3993105fc67
wahidari <wahiid.ari@gmail.com>
2023-03-23 01:15:00 +0700

    update prisma seed

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,5 +1,8 @@
 const { PrismaClient } = require('@prisma/client');
-const { genre, artist, album, song } = require('./data');
+const { genre, artist, album, song, playlist, playlist_song,
+  playlist_user,
+  playlist_user_song,
+  admin } = require('./data');
 const prisma = new PrismaClient();
 
 async function main() {
@@ -38,6 +41,51 @@ async function main() {
     console.log(`Created song with id: ${data.id}`);
   }
   console.log(`Seeding Song Finished.`);
+
+  console.log(`Start Seeding Playlist.`);
+  for (const row of playlist) {
+    const data = await prisma.playlist.create({
+      data: row,
+    });
+    console.log(`Created Playlist with id: ${data.id}`);
+  }
+  console.log(`Seeding Playlist Finished.`);
+  
+  console.log(`Start Seeding Playlist Song.`);
+  for (const row of playlist_song) {
+    const data = await prisma.playlist_song.create({
+      data: row,
+    });
+    console.log(`Created Playlist Song with id: ${data.id}`);
+  }
+  console.log(`Seeding Playlist Song Finished.`);
+  
+  console.log(`Start Seeding Playlist User.`);
+  for (const row of playlist_user) {
+    const data = await prisma.playlist_user.create({
+      data: row,
+    });
+    console.log(`Created Playlist User with id: ${data.id}`);
+  }
+  console.log(`Seeding Playlist User Finished.`);
+  
+  console.log(`Start Seeding Playlist User Song.`);
+  for (const row of playlist_user_song) {
+    const data = await prisma.playlist_user_song.create({
+      data: row,
+    });
+    console.log(`Created Playlist User Song with id: ${data.id}`);
+  }
+  console.log(`Seeding Playlist User Song Finished.`);
+  
+  console.log(`Start Seeding Admin.`);
+  for (const row of admin) {
+    const data = await prisma.admin.create({
+      data: row,
+    });
+    console.log(`Created Admin with id: ${data.id}`);
+  }
+  console.log(`Seeding Admin Finished.`);
 
   console.log(`All Seeding finished.`);
 }
